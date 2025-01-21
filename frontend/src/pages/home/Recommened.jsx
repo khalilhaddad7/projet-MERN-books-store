1bcfdcfaa4d9aa187cb650a9033c810e3bb44a20
khalilhaddad7 <khalilhaddad777@gmail.com>
2025-01-22 00:50:33 +0100

    recomended

--- frontend/src/pages/home/Recommened.jsx
+++ frontend/src/pages/home/Recommened.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+import BookCard from '../books/BookCard';
 
 const Recommened = () => {
+   const [books, setbooks] = useState([]);
+     const [swiperInstance, setSwiperInstance] = useState(null); // Pour accéder au contrôleur de Swiper
+   
+
+
+    useEffect(() => {
+      fetch('books.json')
+        .then((res) => res.json())
+        .then((data) => setbooks(data));
+    }, []);
+
+    const handleSlideChange = (swiper) => {
+      // Si l'utilisateur est à la dernière diapositive
+      if (swiper.isEnd) {
+        setTimeout(() => {
+          swiper.slideTo(0); // Retourner à la première diapositive
+        }, 500); // Délai pour éviter un changement trop brusque
+      }
+    };
+  
   return (
-    <div>Recommenedddd</div>
+    <div className=' py-20'>
+      <h2 className="text-3xl font-semibold mb-6">Recomended for you</h2>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        navigation={true}
+        onSwiper={setSwiperInstance} // Enregistre l'instance Swiper
+        onSlideChange={handleSlideChange} // Détecte les changements de diapositives
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {books.length > 0 &&
+          books.slice(8,18).map((book, index) => (
+            <SwiperSlide key={index}>
+              <BookCard book={book} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
   )
 }
 
