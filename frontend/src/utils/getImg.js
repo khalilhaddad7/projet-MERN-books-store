beebc43e5c6953cd8f7d595233d36cb2a262a658
khalilhaddad7 <khalilhaddad777@gmail.com>
2025-01-29 17:17:56 +0100

    img

--- frontend/src/utils/getImg.js
+++ frontend/src/utils/getImg.js
@@ -1,7 +1,7 @@
 function getImgUrl(name) {
     try {
       // Crée l'URL en fonction du chemin donné
-      return new URL(`../pages/books/${name}`, import.meta.url);
+      return new URL(`../assets/books/${name}`, import.meta.url);
     } catch (error) {
       console.error("Erreur lors de la génération de l'URL :", error);
       return ""; // Retourne une chaîne vide en cas d'erreur
