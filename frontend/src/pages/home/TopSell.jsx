cf1c5c93d7dab98d37f8d3838807149da8edc8a6
khalilhaddad7 <khalilhaddad777@gmail.com>
2025-02-04 18:21:48 +0100

    top sell

--- frontend/src/pages/home/TopSell.jsx
+++ frontend/src/pages/home/TopSell.jsx
@@ -10,19 +10,16 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 // import required modules
 import { Pagination, Navigation } from 'swiper/modules';
+import { useFetchAllBooksQuery } from '../../redux/features/books/booksApi';
 
 const categories = ['Choose a genre', 'Business', 'Fiction', 'Horror', 'Adventure'];
 
 const TopSell = () => {
-  const [books, setbooks] = useState([]);
   const [selectedCategory, setselectedCategory] = useState('choose a genre');
   const [swiperInstance, setSwiperInstance] = useState(null); // Pour accéder au contrôleur de Swiper
-
-  useEffect(() => {
-    fetch('books.json')
-      .then((res) => res.json())
-      .then((data) => setbooks(data));
-  }, []);
+  const {data: books = []} = useFetchAllBooksQuery() ;
+  console.log(books)
+  
 
   const filterbooks =
     selectedCategory === 'choose a genre'
