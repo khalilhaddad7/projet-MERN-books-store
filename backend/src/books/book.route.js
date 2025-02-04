2b380d0ae3518ba4279fcbbbbef3ad85a82a65d5
khalilhaddad7 <khalilhaddad777@gmail.com>
2025-02-04 18:19:58 +0100

    api routes

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,9 +1,29 @@
 const express = require ("express")
 const router = express.Router();
+exports.router = router;
+const {postedbook,getallbooks,deletebook,getSingleBook,updateBook} = require("./book.controller")
 
 // post a book 
-router.post("/create-book",async (req,res)=>{
-    console.log(req.body)
+router.post("/create-book",postedbook )
 
-})
+// get all books
+router.get("/get-all-books",getallbooks)
+
+// delete a single book
+router.delete("/delete-book/:id",deletebook)
+
+//get a single book 
+router.get("/get-single-book/:id",getSingleBook)
+
+//update a book
+router.put("/update-book/:id",updateBook)
 module.exports = router 
+
+
+
+
+
+
+
+
+
